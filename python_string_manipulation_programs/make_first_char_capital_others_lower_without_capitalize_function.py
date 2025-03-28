e4081a635eb610a91b80ec71b31e8f29918bdcd2
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 16:06:55 +0800

    convert the first letter to upper and all others to lower casing

--- python_string_manipulation_programs/make_first_char_capital_others_lower_without_capitalize_function.py
+++ python_string_manipulation_programs/make_first_char_capital_others_lower_without_capitalize_function.py
@@ -1,3 +1,8 @@
 #ask for user input
+word = input('Enter Text: ')
 #capitalize the first letter and lowercase preceeding chars
+if word:
+    result = word[0].upper() + word[1:].lower()
+else:
+    result = word
 #print the result
