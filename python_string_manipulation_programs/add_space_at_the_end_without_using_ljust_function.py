2692446e331f1fe78bedb70682de48174eff2415
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 15:40:24 +0800

    prints the result with description

--- python_string_manipulation_programs/add_space_at_the_end_without_using_ljust_function.py
+++ python_string_manipulation_programs/add_space_at_the_end_without_using_ljust_function.py
@@ -3,6 +3,6 @@ word = input('Enter Text: ')
 lenght = int(input('Enter desired lenght: '))
 #add spaces as needed
 if len(word) < lenght:
-    word += '1' * (lenght - len(word))
+    word += ' ' * (lenght - len(word))
 #print the result
-print(word)
+print('Text after adding spaces: ' , word)
