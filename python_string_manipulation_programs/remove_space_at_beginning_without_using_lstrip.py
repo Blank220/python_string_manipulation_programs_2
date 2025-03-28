01e4a09603a1fbd94ab71a09c55f5b42aa4f8468
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:15:13 +0800

    removed leading spaces

--- python_string_manipulation_programs/remove_space_at_beginning_without_using_lstrip.py
+++ python_string_manipulation_programs/remove_space_at_beginning_without_using_lstrip.py
@@ -1,3 +1,8 @@
 #ask for user input
+word = input('Enter text: ')
 #remove leading spaces without using lstrip
+for i in range(len(word)):
+    if word[i] != ' ':
+        break
 #print result
+print('String without leading spaces:' , word[i])
