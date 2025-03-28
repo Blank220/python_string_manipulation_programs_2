e7667e46467cac792c767f701905eb3c8c4f0572
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:36:58 +0800

    converts letters into lowercase

--- python_string_manipulation_programs/converts_to_lower_case_without_lower()_function.py
+++ python_string_manipulation_programs/converts_to_lower_case_without_lower()_function.py
@@ -1,3 +1,10 @@
 #ask for user input and created a variable that stores the result
+word = input('Enter Text: ')
+result = ''
 #convert each letter in lowercase
+for char in word:
+    if 'A' <= char <= 'Z':
+        result += chr(ord(char) + 32)
+    else:
+        result += char
 #print result
