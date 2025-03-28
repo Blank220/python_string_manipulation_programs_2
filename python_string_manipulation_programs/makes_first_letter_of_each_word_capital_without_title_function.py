01890e56eabacb8721cc1cbd4c51418d2b020201
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 16:21:43 +0800

    split the words to make the first letter capital using capitalize

--- python_string_manipulation_programs/makes_first_letter_of_each_word_capital_without_title_function.py
+++ python_string_manipulation_programs/makes_first_letter_of_each_word_capital_without_title_function.py
@@ -1,3 +1,7 @@
-#ask for user input
+#ask for user input and a variable to hold result
+words = input('Enter Text: ')
+result = ''
 #splits the word and capitalize the first letters
+for word in words.split():
+    result += word.capitalize() + ' '
 #print result
