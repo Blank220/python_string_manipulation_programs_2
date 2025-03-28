0e01a60bfbeccdb69f6e3011643475c50580baea
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 15:59:13 +0800

    uses if else function to reverse casing

--- python_string_manipulation_programs/reverse_text_casing_without_using_swapcase_function.py
+++ python_string_manipulation_programs/reverse_text_casing_without_using_swapcase_function.py
@@ -1,3 +1,12 @@
-#ask for user input
+#ask for user input and a variable that stores result
+word = input('Enter Text: ')
+result = ''
 #checks every character in text and reverses the case
+for char in word:
+    if char.islower():
+        result += char.upper()
+    elif char.isupper():
+        result += char.lower()
+    else:
+        result += char
 #print result
