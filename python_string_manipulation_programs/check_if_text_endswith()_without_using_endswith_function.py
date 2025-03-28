3c197dc2fc91f55fb6d0c99b533c9bf2c902a151
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:57:49 +0800

    checks if the text ends with the suffix by -len

--- python_string_manipulation_programs/check_if_text_endswith()_without_using_endswith_function.py
+++ python_string_manipulation_programs/check_if_text_endswith()_without_using_endswith_function.py
@@ -1,3 +1,6 @@
 #ask for user input and a suffix to check 
+word = input('Enter Text: ')
+suffix = input('Enter suffix to be check: ')
 #checks if the text ends with the suffix provided
+result = word[-len(suffix):] == suffix
 #print the result
