f26981d76e7dc293b7cbfe3c9870aeec626075f2
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:46:29 +0800

    checks if there are any lower case letters and then change the value of uppercase variable if there is

--- python_string_manipulation_programs/checks_if_text_is_upper_case_without_uppercase_function.py
+++ python_string_manipulation_programs/checks_if_text_is_upper_case_without_uppercase_function.py
@@ -1,3 +1,9 @@
-#asks for user input
+#asks for user input and a variable that contains boolean for checking
+word = input('Enter Text: ')
+upper_case = True
 #checks if char is upper
+for char in word:
+    if 'a' <= char <= 'z':
+        upper_case = False
+        break
 #print result
