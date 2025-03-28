0a4d778c7a1401f18f80b9f49274faeceec60fa4
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 15:38:53 +0800

    add spaces depending on the given lenght value

--- python_string_manipulation_programs/add_space_at_the_end_without_using_ljust_function.py
+++ python_string_manipulation_programs/add_space_at_the_end_without_using_ljust_function.py
@@ -1,3 +1,8 @@
 #ask for user input and the desired limit
+word = input('Enter Text: ')
+lenght = int(input('Enter desired lenght: '))
 #add spaces as needed
+if len(word) < lenght:
+    word += '1' * (lenght - len(word))
 #print the result
+print(word)
