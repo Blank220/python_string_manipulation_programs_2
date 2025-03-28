41249272d47c4dbb4469bd1c55fe93bcd54c6ac0
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:29:14 +0800

    finished p2,used startswith and len to remove prefix

--- python_string_manipulation_programs/remove_prefix_without_using_removeprefix_function.py
+++ python_string_manipulation_programs/remove_prefix_without_using_removeprefix_function.py
@@ -1,3 +1,8 @@
 #ask user to input text and the prefix to be remove
+word = input('Enter Text: ')
+prefix = input('Enter the prefix to be remove: ')
 #remove the prefix
+if word.startswith(prefix):
+    word = word[len(prefix):]
 #print result
+print('Removed the prefix:' , word)
