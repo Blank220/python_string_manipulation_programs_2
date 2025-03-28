43151d744dfe34c27e47c523d890881037363230
Mark Comia <marklestercomia22@gmail.com>
2025-03-28 14:59:53 +0800

    prints the result if it is true or false

--- python_string_manipulation_programs/check_if_text_endswith()_without_using_endswith_function.py
+++ python_string_manipulation_programs/check_if_text_endswith()_without_using_endswith_function.py
@@ -4,3 +4,4 @@ suffix = input('Enter suffix to be check: ')
 #checks if the text ends with the suffix provided
 result = word[-len(suffix):] == suffix
 #print the result
+print('The word ends with suffix?: ' , result)
