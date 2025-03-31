d273f2dbb00cf46b4518b37998b268bce0631f18
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:17:26 +0800

    checks all the character and if one is in upper case,returns false otherwise true

--- python_string_manipulation_programs_v2/checks_if_all_char_are_lower_case_without_islower_function.py
+++ python_string_manipulation_programs_v2/checks_if_all_char_are_lower_case_without_islower_function.py
@@ -1,3 +1,9 @@
 #asks for user input
+word = input('Enter Text: ')
 #checks if all chars is in lowercase
+is_lower = True
+for char in word:
+    if 'A' <= char <= 'Z':
+        is_lower = False
+        break
 #prints the boolean result
