ed4d66f1bedc2d894c653c37dba607890e321623
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:31:12 +0800

    checks if the given prefix matches the starting word

--- python_string_manipulation_programs_v2/checks_if_text_starts_with_parameter_wihout_using_startswith_function.py
+++ python_string_manipulation_programs_v2/checks_if_text_starts_with_parameter_wihout_using_startswith_function.py
@@ -1,3 +1,6 @@
 #asks for user input and another input for checking
+word = input('Enter Text: ')
+starts = input('Enter prefix to check: ')
 #checks if text starts with the given input
+starts_with = word[:len(starts)] == starts
 #prints the result
