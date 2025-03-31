be28a2f784ac4406a056937c05a3222bd6c8dac3
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:40:56 +0800

    adds spaces as indicated by the lenght at the start

--- python_string_manipulation_programs_v2/add_spaces_at_start_without_using_rjust_function.py
+++ python_string_manipulation_programs_v2/add_spaces_at_start_without_using_rjust_function.py
@@ -1,3 +1,7 @@
 #asks for user input and lenght of spaces
+word = input('Enter Text: ')
+lenght = int(input('Enter desired lenght: '))
 #add spaces at the start as needed
+if len(word) < lenght:
+    word = ' ' * (lenght - len(word)) + word
 #prints the result with spaces in beginning
