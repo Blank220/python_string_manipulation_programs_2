7ee94fcb7c53af400db622dd2c212669fa6cd62c
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:57:25 +0800

    manually count the appearance of the given char using for loop

--- python_string_manipulation_programs_v2/counts_how_many_times_a_char_appear_without_count_function.py
+++ python_string_manipulation_programs_v2/counts_how_many_times_a_char_appear_without_count_function.py
@@ -1,3 +1,9 @@
 #ask for user input and a char to count
+word = input('Enter Text: ')
+char = input('Enter a character to count:')
 #count how many times the char appeared in the string
+count = 0
+for c in word:
+    if c == char:
+        count += 1
 #prints the number of times it appeared
