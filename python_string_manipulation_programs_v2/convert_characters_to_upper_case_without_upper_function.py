295bec78a8bc25aef3b5e3445373eb761f0d6335
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:09:19 +0800

    ask for user input then converts into all upper casing

--- python_string_manipulation_programs_v2/convert_characters_to_upper_case_without_upper_function.py
+++ python_string_manipulation_programs_v2/convert_characters_to_upper_case_without_upper_function.py
@@ -1,3 +1,11 @@
-#ask for user input
+#ask for user input and a variable for storing result
+word = input('Enter Text: ')
+result = ''
 #converts the given text into upper case without upper function
+for char in word:
+    if 'a' <= char <= 'z':
+        result += chr(ord(char) -32)
+    else:
+        result += char
+
 #prints the result, text in upper casing
