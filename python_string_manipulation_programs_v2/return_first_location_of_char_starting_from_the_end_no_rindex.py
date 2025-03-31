37b15c70f6bfd2eeba6fff48e0516c1fff79e21e
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 17:26:01 +0800

    Finished the program,prints the last occrence of the char

--- python_string_manipulation_programs_v2/return_first_location_of_char_starting_from_the_end_no_rindex.py
+++ python_string_manipulation_programs_v2/return_first_location_of_char_starting_from_the_end_no_rindex.py
@@ -1,2 +1,10 @@
 #ask for user input and character to locate
+word = input('Enter Text: ')
+char = input('Character to find: ')
 #finds the character's index starting from last
+for i in range(len(word) -1, -1, -1):
+    if word[i] == char:
+        print(f'{char} found at index {i}')
+        break
+else:
+    print(f'{char} is not in text')
