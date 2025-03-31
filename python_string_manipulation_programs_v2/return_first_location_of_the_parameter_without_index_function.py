ad7a22c0499bb12ddf7daac2f8e7e8904221c0bc
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 17:08:22 +0800

    checks for the first appearance of the char

--- python_string_manipulation_programs_v2/return_first_location_of_the_parameter_without_index_function.py
+++ python_string_manipulation_programs_v2/return_first_location_of_the_parameter_without_index_function.py
@@ -1,3 +1,10 @@
 #ask for user input and character to find
+word = input('Enter Text: ')
+char = input('Enter character to find: ')
 #Find the first appearance of char
+for i in range(len(word)):
+    if word[i] == char:
+        print(f'{char} found at index {i}')
+    else:
+        print(f'{char} is not in text')
 #prints result
