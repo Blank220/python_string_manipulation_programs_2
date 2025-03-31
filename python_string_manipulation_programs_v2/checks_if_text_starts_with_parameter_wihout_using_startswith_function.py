72e84b52deece70ec484985f72a0a724135d1cf0
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:33:20 +0800

    prints the result,true if it matches and false if it does not match

--- python_string_manipulation_programs_v2/checks_if_text_starts_with_parameter_wihout_using_startswith_function.py
+++ python_string_manipulation_programs_v2/checks_if_text_starts_with_parameter_wihout_using_startswith_function.py
@@ -4,3 +4,4 @@ starts = input('Enter prefix to check: ')
 #checks if text starts with the given input
 starts_with = word[:len(starts)] == starts
 #prints the result
+print('Text starts with the prefix??: ', starts_with)
