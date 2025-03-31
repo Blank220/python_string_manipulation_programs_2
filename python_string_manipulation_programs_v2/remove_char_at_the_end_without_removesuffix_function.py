5324ce6d7bf70f97a07645955b7eae58383c8d6d
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 15:48:45 +0800

    asks for user inputs and then remove given suffix from the word

---
+++ python_string_manipulation_programs_v2/remove_char_at_the_end_without_removesuffix_function.py
@@ -0,0 +1,7 @@
+#asks for user input and suffix to be remove
+word = input('Enter Text: ')
+suffix = input('Enter suffix to be remove: ')
+#removes the suffix indicated
+if word.endswith(suffix):
+    word = word[:-len(suffix)]
+#prints the result
