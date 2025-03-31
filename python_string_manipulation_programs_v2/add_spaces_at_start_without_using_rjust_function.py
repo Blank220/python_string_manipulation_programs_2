4302116193f9c73fa909a4d4eebccafe05256d76
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:42:33 +0800

    prints result after right justification

--- python_string_manipulation_programs_v2/add_spaces_at_start_without_using_rjust_function.py
+++ python_string_manipulation_programs_v2/add_spaces_at_start_without_using_rjust_function.py
@@ -5,3 +5,4 @@ lenght = int(input('Enter desired lenght: '))
 if len(word) < lenght:
     word = ' ' * (lenght - len(word)) + word
 #prints the result with spaces in beginning
+print('Text after right justification: ', word)
