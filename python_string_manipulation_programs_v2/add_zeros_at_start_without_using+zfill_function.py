8e6ae95318eb2f9e41658bbc540929e991bc8b25
Mark Comia <marklestercomia22@gmail.com>
2025-03-31 16:49:35 +0800

    Add zeros at start depending on user lenght input

--- python_string_manipulation_programs_v2/add_zeros_at_start_without_using+zfill_function.py
+++ python_string_manipulation_programs_v2/add_zeros_at_start_without_using+zfill_function.py
@@ -1,3 +1,7 @@
 #asks for user input and desired lenght of string
+text = input('Enter String: ')
+lenght = int(input('Enter desired lenght: '))
 #add zeros at the start without zfill()
+if len(text) < lenght:
+    text = '0' * (lenght -len(text)) + text
 #prints the result with zeros
